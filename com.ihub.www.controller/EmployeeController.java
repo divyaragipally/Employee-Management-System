1b36644cc12a6deb62ad5dac88740b6ef4162024
divyaragipally <150668073+divyaragipally@users.noreply.github.com>
2026-07-21 22:26:40 +0530

    Create EmployeeController.java

---
+++ com.ihub.www.controller/EmployeeController.java
@@ -0,0 +1,58 @@
+package com.ihub.www.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ihub.www.entity.Employee;
+import com.ihub.www.service.EmployeeService;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController
+{
+	@Autowired
+	EmployeeService employeeService;
+	
+	@PostMapping("/employee")
+	public Employee addEmployee(@RequestBody Employee employee)
+	{
+		return employeeService.addEmployee(employee);
+	}
+	
+	@GetMapping("/employee")
+	public List<Employee> getAllEmployees()
+	{
+		return employeeService.getAllEmployees();
+	}
+	
+	@GetMapping("/employee/{id}")
+	public Employee getEmployeeById(@PathVariable("id") long id)
+	{
+		return employeeService.getEmployeeById(id);
+	}
+	
+	@PutMapping("/employee/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long id,@RequestBody Employee employee)
+	{
+		return employeeService.updateEmployee(id, employee);
+	}
+	
+	@DeleteMapping("/employee/{id}")
+	public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable("id") long id)
+	{
+		return employeeService.deleteEmployee(id);
+	}
+}
